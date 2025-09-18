7a8cff10fabc56b640ba401b64ed1e7c953365f2
moradahmadnasab <morad.ahmadnasab@gmail.com>
2025-09-18 16:47:50 -0400

    Add files via upload

---
+++ mycodes/OSDR1.m
@@ -0,0 +1,48 @@
+function [XX,ei111] = OSDR1(A,B)
+% OSDR1 is an implementation of Algorithm 1 which is used with 
+% the same name in the numerical experiment section of the associated paper. 
+% Inputs: Square matrices A and B with the same size.
+% Outputs: Eigenpair set of (A,B), namely, XX for the set of eigenvectors 
+% and ei111 for the set of eigenvalues.
+% Written by :Morad Ahmadnasab, 5/10/2022, University of Kurdistan, Sanandaj, Iran.
+%%%%%%%%%%%
+
+%%%% Step 1:
+
+[V,D]=eig(B);
+d=diag(D);
+maxd=max(d);
+mind=min(d);
+maxdmind=maxd/mind;
+if abs(maxdmind) <1e+10
+  [di,Id]=sort(d,'descend');
+else
+  [di,Id]=sort(d,'ascend');
+end
+s1=diag(di);
+u1=V(:,Id);
+
+%%%%  Step 2 together with symmetrizing discussed in Section 5.3:
+
+aa=u1'*A*u1;
+
+%
+n=length(A);
+for k1=2:n
+    for k2=1:k1-1
+         aa(k2,k1)=aa(k1,k2);
+    end
+end
+
+%%%%  Step 3: 
+
+[X,eigM]=eig(aa,s1,'chol');
+
+%%%%  Step 4:
+
+ei111= diag(eigM);
+
+XX=u1*X;
+end
+
+
---
+++ mycodes/RSSEP1.m
@@ -0,0 +1,53 @@
+function [XX,ei111] = RSSEP1(A,B)
+% RSSEP1 is an implementation of Algorithm 2 which is used with 
+% the same name in the numerical experiment section of the associated paper.   
+% Inputs: Square matrices A and B with the same size.
+% Outputs: Eigenpair set of (A,B), namely, XX for eigenvectors and ei111 for eigenvalues.
+% Written by :Morad Ahmadnasab, 5/10/2022, university of Kurdistan, Sanandaj, Iran.
+%%%%%%%%%%%
+
+%%%% Step 1:
+
+n=length(B);
+[V,D]=eig(B);
+d=diag(D);
+maxd=max(d);
+mind=min(d);
+maxdmind=maxd/mind;
+if abs(maxdmind) <1e+10
+   [di,Id]=sort(d,'descend');
+else
+   [di,Id]=sort(d,'ascend');
+end
+
+u1=V(:,Id);
+
+%%%% Step 2 together with symmetrizing discussed in Section 5.3:
+
+s1=diag(sqrt(di));  
+s2=zeros([n n]);
+for i = 1:n
+    s2(i,i)=1/s1(i,i);
+end
+
+aa=(s2*u1')*A*(u1*s2);
+
+%
+for k1=2:n
+    for k2=1:k1-1
+        aa(k2,k1)=aa(k1,k2);
+    end
+end
+
+
+%%%% Step 3:
+
+[X,eigM]=eig(aa);
+
+%%%% Step 4: 
+
+ei111=diag(eigM);
+XX=(u1*s2)*X;
+end
+
+
